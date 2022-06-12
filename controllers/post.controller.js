c0fc4bd57e541324485964bc7c6a847cf8f45428
Rivaldi Palermo <rivaldi.palermo11@gmail.com>
2022-06-12 17:33:53 +0700

    [ADD] Update post method

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,21 +1,43 @@
 const Post = require("../models/Post");
 
 module.exports = {
-	index: async (req, res) => {
-		const posts = await Post.findAll();
-		return res.render("post/index", {
-			posts,
-		});
-	},
-	create: async (req, res) => {
-		return res.render("post/create");
-	},
-	store: async (req, res) => {
-		await Post.create(req.body);
-		return res.redirect("/posts");
-	},
-	delete: async (req, res) => {
-		Post.destroy({ where: { id: req.params.id } });
-		return res.redirect("/posts");
-	},
+  index: async (req, res) => {
+    const posts = await Post.findAll();
+    return res.render("post/index", {
+      posts,
+    });
+  },
+  create: async (req, res) => {
+    return res.render("post/create");
+  },
+  store: async (req, res) => {
+    await Post.create(req.body);
+    return res.redirect("/posts");
+  },
+  show: async (req, res) => {
+    // galih's todo
+  },
+  update: async (req, res) => {
+    const posts = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!posts) {
+      return res.redirect("/posts");
+    }
+
+    await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.redirect("/posts");
+  },
+  delete: async (req, res) => {
+    Post.destroy({ where: { id: req.params.id } });
+    return res.redirect("/posts");
+  },
 };
--- routes/index.route.js
+++ routes/index.route.js
@@ -9,5 +9,8 @@ router.get("/", indexController.index);
 router.get("/posts", postController.index);
 router.get("/posts/create", postController.create);
 router.post("/posts/store", postController.store);
+router.get("/posts/:id", postController.show);
+router.put("/posts/:id", postController.update);
 router.delete("/posts/:id/delete", postController.delete);
+
 module.exports = router;
